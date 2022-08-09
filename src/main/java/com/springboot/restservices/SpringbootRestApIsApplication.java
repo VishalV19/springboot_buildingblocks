388200cc7f6ac4be34659766c99bf6f6d9dcdf78
Vishal Singh <vishalsingh.vh99@gmail.com>
2022-08-10 00:26:24 +0530

    First commit

---
+++ src/main/java/com/springboot/restservices/SpringbootRestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApIsApplication.class, args);
+	}
+
+}
